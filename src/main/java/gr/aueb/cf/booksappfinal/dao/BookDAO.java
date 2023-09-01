75156964191e287d4b22a377643704e9dbc707a2
ckontos <har.kontos@hotmail.com>
2023-09-01 18:32:24 +0300

    Changed DAO interface

--- src/main/java/gr/aueb/cf/booksappfinal/dao/BookDAO.java
+++ src/main/java/gr/aueb/cf/booksappfinal/dao/BookDAO.java
@@ -1,9 +1,7 @@
 package gr.aueb.cf.booksappfinal.dao;
 
 import gr.aueb.cf.booksappfinal.entities.Book;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.lang.Nullable;
 
 import java.util.List;
 
