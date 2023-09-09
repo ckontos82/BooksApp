da498ae17f2c4dfef24766397f5a52ce6f4cf9f8
ckontos <har.kontos@hotmail.com>
2023-09-09 07:36:18 +0300

    Minor changes.

--- src/main/java/gr/aueb/cf/booksappfinal/config/SecurityConfig.java
+++ src/main/java/gr/aueb/cf/booksappfinal/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 //@EnableWebSecurity
